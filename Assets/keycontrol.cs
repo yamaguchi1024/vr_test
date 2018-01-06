e2144e949310963899ff97a8968377775115dcab
yamaguchi1024 <yukatkh@yahoo.co.jp>
2018-01-07 00:04:31 +0900

    first commit

---
+++ Assets/keycontrol.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keycontrol : MonoBehaviour {
+
+  // Use this for initialization
+  void Start () {
+  }
+
+  private float x;
+  private float y;
+  private Vector3 rotateValue;
+
+  void Update()
+  {
+    transform.position += transform.forward * Input.GetAxis("Vertical");
+    transform.position += transform.right * Input.GetAxis("Horizontal");
+
+    y = Input.GetAxis("MouseX");
+    x = Input.GetAxis("MouseY");
+    Debug.Log(x + ":" + y);
+    rotateValue = new Vector3(x, y * -1, 0);
+    transform.eulerAngles = transform.eulerAngles - rotateValue;
+  }
+}
---
+++ Assets/putobject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class putobject : MonoBehaviour {
+
+	// Use this for initialization
+  private GameObject cube;
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    Vector3 pos = transform.position;
+    pos.y = 0.0f;
+    transform.position = pos;
+
+    if (Input.GetKeyDown("space")) {
+      cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+      cube.transform.position = transform.position + transform.forward;
+    }
+	}
+}
